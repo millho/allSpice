4c6cb04ae59cc0892475d866645c74f03a7af34c
Jason Millhorn <jdmillhorn@gmail.com>
2023-10-18 13:58:33 -0600

    commit

---
+++ client/src/components/RecipeForm.vue
@@ -0,0 +1,25 @@
+<template>
+    <form @submit="createRecipe()">
+        <input v-model="recipeData.title" type="text" placeholder="Title">
+        <input v-model="recipeData.instructions" type="text" placeholder="Recipe Instructions">
+        <input v-model="recipeData.img" type="text" placeholder="Recipe Image URL">
+        <input v-model="recipeData.category" type="text">
+    </form>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+    setup() {
+        const recipeData = ref({});
+
+        return {
+            recipeData,
+        };
+    },
+};
+</script>
+
+
+<style></style>
---
+++ client/src/components/SearchBar.vue
@@ -0,0 +1,16 @@
+<template>
+    <section class="row">
+
+    </section>
+</template>
+
+<script>
+export default {
+    setup() {
+        return {};
+    },
+};
+</script>
+
+
+<style></style>
